ecb2a9ed86f0870697bf7c4b855fcbe4875e72a0
yousef-asfan <88773904+yousef-asfan@users.noreply.github.com>
2023-02-05 14:14:45 +0300

    add cube

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { TextureLoader } from 'three';
 
 const scene = new THREE.Scene();
 const sizes = {
@@ -20,6 +21,7 @@ const renderer = new THREE.WebGLRenderer({
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.dampingFactor = 0.1;
+controls.target.set(0,0,0);
 
 const loadingManager = new THREE.LoadingManager(
   //loaded
@@ -36,7 +38,7 @@ const loadingManager = new THREE.LoadingManager(
 
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setSize(sizes.width, sizes.height)
-camera.position.z = 1;
+camera.position.set(0,5,5);
 scene.add(camera);
 
 // Sizes
@@ -55,9 +57,33 @@ const dracoLoader = new DRACOLoader(loadingManager);
 dracoLoader.setDecoderPath('/draco/');
 const gltfLoader = new GLTFLoader(loadingManager);
 gltfLoader.setDRACOLoader(dracoLoader);
+const textureLoader = new THREE.TextureLoader(loadingManager);
 
-const gridHelper = new THREE.GridHelper(5,5);
-scene.add(gridHelper);  
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const cubeMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(2,2,2),
+  new THREE.MeshBasicMaterial({color: 0xff00ff})
+);
+cubeMesh.position.y = 1;
+scene.add(cubeMesh);
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const gridHelper = new THREE.GridHelper(10,10);
+scene.add(gridHelper);
 
 const clock = new THREE.Clock();
 const tick = () => {
