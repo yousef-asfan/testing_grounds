bb552ad7cfc946b947dfebe04997e12384c7890c
yousef-asfan <88773904+yousef-asfan@users.noreply.github.com>
2023-02-12 11:06:03 +0300

    test youtube video

--- main.js
+++ main.js
@@ -1,96 +1,125 @@
-import './style.css'
+/* global THREE */
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-import { TextureLoader } from 'three';
 
-const scene = new THREE.Scene();
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
+let scene;
+let camera;
+let renderer;
+let cube;
+let plane1;
+let plane2;
+
+function setup() {
+  setupScene();
+  setupRenderer();
+  setupEventListeners();
+  setupCamera();
+  setupLights();
+
+  let video1 = setUpVideo(
+    "https://www.youtube.com/embed/anEAQ4epzoI?mute=1&autoplay=1"
+  );
+  // let video2 = setUpVideo(
+  //   "https://cdn.glitch.com/39b7ba95-a96e-44aa-9110-0d917a3046ad%2FOutro_Vid_v2.mp4?v=1596058679403"
+  // );
+
+  let texture1 = createTextureFromVideoElement(video1);
+  // let texture2 = createTextureFromVideoElement(video2);
+  // texture.crossOrigin = "anonymous";
+
+  plane1 = new THREE.Mesh(
+    new THREE.PlaneGeometry(.5, .5),
+    new THREE.MeshBasicMaterial({ map: texture1, side: THREE.DoubleSide })
+    
+  );
+
+  scene.add(plane1);
+  draw();
 }
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
-const canvas = document.querySelector('canvas.app');
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true
-});
+function setupScene() {
+  scene = new THREE.Scene();
+}
 
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.dampingFactor = 0.1;
-controls.target.set(0,0,0);
+function setupCamera() {
+  let res = window.innerWidth / window.innerHeight;
+  camera = new THREE.PerspectiveCamera(75, res, 0.1, 1000);
+  camera.position.z = 1;
+}
 
-const loadingManager = new THREE.LoadingManager(
-  //loaded
-  () => {
-  },
-  //progress
-  (itemURL, itemsLoaded, itemsTotal) => {
-  },
-  //error
-  () => {
-    console.log(urlError);
-  }
-);
+// https://stackoverflow.com/questions/19251983/dynamically-create-a-html5-video-element-without-it-being-shown-in-the-page/20611625
 
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setSize(sizes.width, sizes.height)
-camera.position.set(0,5,5);
-scene.add(camera);
+function setUpVideo(inSrc) {
+  var videlem = document.createElement("video");
+  /// ... some setup like poster image, size, position etc. goes here...
+  /// now, add sources:
+  var sourceMP4 = document.createElement("source");
+  sourceMP4.type = "video/mp4";
+  sourceMP4.src = inSrc;
+  // performance
+  
 
-// Sizes
-window.addEventListener('resize', () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+  videlem.appendChild(sourceMP4);
 
-  camera.aspect = sizes.width / sizes.height;
+  videlem.autoplay = true;
+  videlem.muted = true;
+  videlem.setAttribute("crossorigin", "anonymous");
+  // i think this will not be not be needed if you have a server
+
+  videlem.style.display = "none"; // this makes it so the html element isnt there
+
+  videlem.load();
+  videlem.play();
+  return videlem;
+}
+
+function createTextureFromVideoElement(video) {
+  let texture = new THREE.VideoTexture(video);
+  texture.crossOrigin = "anonymous";
+  texture.needsUpdate;
+  texture.minFilter = THREE.LinearFilter;
+  texture.magFilter = THREE.LinearFilter;
+  // texture.format = THREE.RGBFormat;
+  return texture;
+}
+
+function setupRenderer() {
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
+
+function setupLights() {
+  let ambientLight = new THREE.AmbientLight(0x0c0c0c);
+  scene.add(ambientLight);
+
+  let spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(-30, 60, 60);
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+}
+
+function setupEventListeners() {
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-const dracoLoader = new DRACOLoader(loadingManager);
-dracoLoader.setDecoderPath('/draco/');
-const gltfLoader = new GLTFLoader(loadingManager);
-gltfLoader.setDRACOLoader(dracoLoader);
-const textureLoader = new THREE.TextureLoader(loadingManager);
-
-/**
- * 
- * 
- * 
- * 
- * 
- */
-
-const cubeMesh = new THREE.Mesh(
-  new THREE.BoxGeometry(2,2,2),
-  new THREE.MeshBasicMaterial({color: 0xffffff})
-);
-cubeMesh.position.y = 1;
-scene.add(cubeMesh);
-
-/**
- * 
- * 
- * 
- * 
- * 
- */
-
-const gridHelper = new THREE.GridHelper(10,10);
-scene.add(gridHelper);
-
-const clock = new THREE.Clock();
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  controls.update();
-
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(tick);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
-tick();
+
+function draw() {
+  requestAnimationFrame(draw);
+  renderer.render(scene, camera);
+
+  let angle = 0.007;
+  // plane1.rotation.x += angle;
+  // plane1.rotation.y += angle * 0.125;
+  
+  // plane2.rotation.x -= angle;
+  // plane2.rotation.y -= angle * 0.99;
+  
+ // camera.position.x += 0.01;
+}
+
+setup();
